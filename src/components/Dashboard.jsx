cd10acd57c8193c1a970d3f26f0212906b496b66
Andres <al20760275@ite.edu.mx>
2024-10-03 14:42:37 -0700

    cambio de server y configuracion

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchRiegoData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/riego');
+        const response = await axios.get('http://localhost:5000/api/registro_riego'); // Cambia esta URL si es necesario
         setRiegoData(response.data);
       } catch (error) {
         console.error('Error obteniendo los datos de riego:', error);
@@ -55,25 +55,31 @@ const Dashboard = () => {
         </ul>
       </aside>
       <main className="main-content">
-        {/* <h2>Historial de Riegos</h2> */}
+        <h2>Historial de Riegos</h2>
         <table>
-          {/* <thead>
+          <thead>
             <tr>
               <th>Fecha</th>
               <th>Hora</th>
               <th>Veces Regadas</th>
-              <th>Cantidad de Agua</th>
+              <th>Cantidad de Agua (L)</th>
             </tr>
-          </thead> */}
+          </thead>
           <tbody>
-            {riegoData.map((riego, index) => (
-              <tr key={index}>
-                <td>{riego.fecha}</td>
-                <td>{riego.hora}</td>
-                <td>{riego.cantidad_de_veces_regadas}</td>
-                <td>{riego.cantidad_de_agua_regada}</td>
+            {riegoData.length > 0 ? (
+              riegoData.map((riego, index) => (
+                <tr key={index}>
+                  <td>{riego.fecha}</td>
+                  <td>{riego.hora}</td>
+                  <td>{riego.cantidad_de_veces_regada}</td>
+                  <td>{riego.cantidad_de_agua_regada}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="4">No hay datos de riego disponibles.</td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
         <Outlet />
